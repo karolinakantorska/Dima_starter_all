6c479fa8a8fc886c41c6afa14603bc686226bce8
karolina <karolina.kantorska@gmail.com>
2022-04-26 20:20:11 +0200

    one project added deploy 5

--- src/pages/unternehmen/mitarbeitern.tsx
+++ src/pages/unternehmen/mitarbeitern.tsx
@@ -2,7 +2,8 @@
 import Page from '../../components/Page';
 import { RootStyle } from 'src/components/_Main/RootStyle';
 import Layout from 'src/layouts';
-import { PeopleCom } from '../../components/_Company/peopleCom';
+import { Grid, Typography, Container } from '@mui/material';
+//import { PeopleCom } from '../../components/_Company/peopleCom';
 
 
 
@@ -11,7 +12,19 @@ export default function Mitarbeitern() {
     <Layout>
       <Page title="Dima & Partner | Glarus Zurich Arosa">
         <RootStyle>
-          <PeopleCom />
+
+          <Container>
+            <Grid container direction="row" justifyContent="center" spacing={4}>
+              <Grid item>
+                <Typography variant="h6" component="h1" paragraph color="dima">
+                  Mitarbeitern
+                </Typography>
+                <Typography gutterBottom>
+                  Text
+                </Typography>
+              </Grid>
+            </Grid>
+          </Container>
         </RootStyle>
       </Page>
     </Layout>
