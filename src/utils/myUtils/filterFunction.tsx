65b1b19dd1912aa0794255347f06c4aa8acc4490
karolina <karolina.kantorska@gmail.com>
2022-05-02 14:37:17 +0200

    filter Referenzen deployment 2

--- src/utils/myUtils/filterFunction.tsx
+++ src/utils/myUtils/filterFunction.tsx
@@ -21,20 +21,7 @@ export function filter(allProjects: ProjectsListType, { param }: { param: Phase 
         return allProjects;
     }
 }
-export function filter2(allProjects: ProjectsListType, { param }: { param: Phase | Regionen }) {
-    if (param === 'Alle') {
-        return allProjects;
-    }
-    if (param === "Entwicklung" || "Plannung" || "Realisierung") {
-        console.log('param: ', param)
-        return allProjects.filter((project) => project.phase.includes(param));
-    }
-    if (param === "Glarus" || "Zürich" || "Arosa" || "Andere Regionen") {
-        console.log('param: ', param)
-        //return allProjects;
-        return allProjects.filter((project) => project.region.includes(param));
-    }
-}
+
 export function filterMany(allProjects: ProjectsListType, { phase, region }: FilterParams) {
     /*
     function filterAlter(project: ProjectType) {
